85cd8920a573e8601a3edc0105e39a1fba25493e
ice <ice_hotf@proton.me>
2025-02-17 11:09:47 +0100

    Update DZL_Install.sh

--- DZL_Install.sh
+++ DZL_Install.sh
@@ -21,12 +21,12 @@ sudo chmod +x /home/$USER/DZL/./*
 echo ""
 echo ""
 ### Copy DZL.desktop file to your application Directory ###
-cp /home/$USER/DZL-Beta-main/DZL/DZL/DZL.desktop /home/$USER/.local/share/applications/DZL.desktop &
+cp /home/$USER/DZL-Beta-main/DZL/DZL.desktop /home/$USER/.local/share/applications/DZL.desktop &
 echo "DZL.desktop was copied to '/home/$USER/.local/share/applications/' and should be available in the application menu."
 echo ""
 echo ""
 ### Copy dzl.png file to your 'Pictures' Directory ###
-cp /home/$USER/DZL-Beta-main/DZL/DZL/dzl.png /home/$USER/Pictures/
+cp /home/$USER/DZL-Beta-main/DZL/dzl.png /home/$USER/Pictures/
 echo "Application Icon copied to 'Pictures'. Make sure to change the 'Icon Path' in the DZL.desktop file."
 echo ""
 echo ""
