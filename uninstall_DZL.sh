ec20eff59e2df852f9e40067648ec49fefc33970
ice <ice_hotf@proton.me>
2025-02-17 12:32:39 +0100

    Update uninstall_DZL.sh

--- uninstall_DZL.sh
+++ uninstall_DZL.sh
@@ -1,10 +1,13 @@
+#!/bin/bash
+
 ### Uninstall DZL###
+
+
 cd /home/$USER/
 sudo rm /home/$USER/.local/share/applications/DZL.desktop 
 sudo rm /usr/share/icons/dzl.png
 sudo rm -rf /home/$USER/DZL/
 sudo rm -rf /home/$USER/DZL-Beta-main/
 sudo rm /home/$USER/DZL-Beta-main.zip
+cd ~
 echo "Uninstallation Successful"
-sleep 2
-exit
