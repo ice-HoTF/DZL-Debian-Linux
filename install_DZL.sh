982af08d39007d6aeda6b9a528586643b7348f81
ice <ice_hotf@proton.me>
2025-02-17 12:23:13 +0100

    Update install_DZL.sh

--- install_DZL.sh
+++ install_DZL.sh
@@ -27,12 +27,10 @@ echo "DZL.desktop was copied to '/home/$USER/.local/share/applications/' and sho
 echo ""
 echo ""
 ### Copy dzl.png file to /usr/share/icons ###
-cp /home/$USER/DZL-Beta-main/DZL/dzl.png /usr/share/icons/
+sudo cp /home/$USER/DZL-Beta-main/DZL/dzl.png /usr/share/icons/
 echo "Application Icon copied to 'usr/share/icons'"
 echo ""
 echo ""
-### Copy Icon to /usr/share/icons/
-sudo cp /home/ice/Pictures/dzl.png /usr/share/icons/
 echo ""
 echo ""
 echo "All Done! Launch DZL by Using the Application Menu Entry or through Terminal: bash /home/$USER/DZL/DZL.sh"
