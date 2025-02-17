43993f721644b2d0b7ba83dbcb79ddeaaa7cc99c
ice <ice_hotf@proton.me>
2025-02-17 12:21:43 +0100

    Add files via upload

---
+++ uninstall_DZL.sh
@@ -0,0 +1,7 @@
+### Uninstall DZL###
+
+sudo rm /usr/share/icons$ sudo rm /home/ice/.local/share/applications/DZL.desktop 
+sudo rm /usr/share/icons$ sudo rm /usr/share/icons
+sudo rm -rf /home/$USER/DZL/
+sudo rm -rf /home/$USER/DZL-Beta-main/
+sudo rm /home/$USER/DZL-Beta-main.zip
