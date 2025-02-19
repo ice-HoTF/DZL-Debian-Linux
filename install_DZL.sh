aa874afd79bc3ac29ad348415930422c21a50041
ice <ice_hotf@proton.me>
2025-02-20 00:09:52 +0100

    Update install_DZL.sh

--- install_DZL.sh
+++ install_DZL.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+
+# Get & set Current Directory as a Variable:
 pwd=$(pwd list)
 
 
@@ -9,6 +11,8 @@ cp -R /$pwd/DZL/ /home/$USER/
 echo ""
 echo ""
 echo "Installing Dependencies:"
+
+
 ### Install Dependencies: ###
 echo ""
 sudo apt install gawk -y
@@ -20,17 +24,23 @@ echo ""
 sudo apt install jq -y
 echo ""
 echo ""
+
+
 ### Give user ownership to .png icon ###
 echo "Taking Ownership of the DZL Folder Contents."
 sudo chmod +x /home/$USER/DZL/./*
 sudo chmod +x /home/$USER/DZL/./*
 echo ""
 echo ""
+
+
 ### Copy DZL.desktop file to your application Directory ###
 cp /home/$USER/DZL/DZL.desktop /home/$USER/.local/share/applications/DZL.desktop &
 echo "DZL.desktop was copied to '/home/$USER/.local/share/applications/' and should be available in the application menu."
 echo ""
 echo ""
+
+
 ### Copy dzl.png file to /usr/share/icons ###
 sudo cp /home/$USER/DZL/dzl.png /usr/share/icons/
 echo "Application Icon copied to 'usr/share/icons'"
